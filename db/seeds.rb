221c318b86344e72b1648e475a88ee8d95c0e29a
kaku ogihara <kaku.ogi@gmail.com>
2017-03-08 00:01:01 +0000

    AddDive09

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  name = Faker::Name.name
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               name: name
+               )
+end
+  n = 8
+  while n <= 107
+    title = Faker::Name.title
+    content = Faker::Food.ingredient
+    Blog.create(
+      title: title,
+      content: content,
+      user_id: n
+    )
+    n = n + 1
+  end
