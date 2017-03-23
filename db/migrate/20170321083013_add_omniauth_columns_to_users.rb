909152a57e84f8dd157e4aad91904f09d9b2fca5
kaku ogihara <kaku.ogi@gmail.com>
2017-03-23 12:42:13 +0000

    migrate_provider_default

--- db/migrate/20170321083013_add_omniauth_columns_to_users.rb
+++ db/migrate/20170321083013_add_omniauth_columns_to_users.rb
@@ -1,7 +1,7 @@
 class AddOmniauthColumnsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :uid, :string, null: false, default: ""
-    add_column :users, :provider, :string, null: false, default: ""
+    add_column :users, :provider, :string, null: false, default: "ppp"
     add_column :users, :image_url, :string
     add_index :users, [:uid, :provider], unique: true
   end
