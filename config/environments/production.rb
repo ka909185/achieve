fae60a12f4c5a640dba3d7fc4fd24a856172d64e
ka909185 <kaku.ogi@gmail.com>
2017-05-05 12:25:30 +0900

    DIVE20-2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'agile-spire-55083.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '52.197.60.88' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
    {
